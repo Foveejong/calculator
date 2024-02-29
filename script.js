83840c0c151a6299579d1433e56bd7773bfe0d1d
foveejong <jovefong62@gmail.com>
2024-02-29 17:13:51 +0800

    Store clicked numbers into first variable

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-let firstVar, opr, secondVar, result;
+let firstVar = "";
+let secondVar = "";
+let result = "";
+let opr = "";
+
+// regex for numeric values and arithmetic operators
+const regex = new RegExp(/[0-9()+\-*=/.]/g);
+const display = document.querySelector("#display");
+const numButtons = document.querySelectorAll(".num");
+
+// for each button add eventlistener for button clicked and display
+numButtons.forEach(key => {
+    key.addEventListener("click", e => {
+        //upon click, let first variable store values and populate display
+        firstVar+=e.target.textContent;
+        display.textContent = firstVar;
+    })
+})
+
+// if user presses valid numeric key, display value
+// display.addEventListener("keypress", e => {
+//     if (regex.test(e.key)) {
+//         display.textContent+= e.key;
+//     }
+// })
 
 function operate(a, b, op) {
     if (op === "+") return add(a, b)
@@ -6,7 +30,6 @@ function operate(a, b, op) {
     if (op === "*") return multiply(a, b)
     if (op === "/") return divide(a, b)
 }
-
 
 function add(a, b) {
     return a + b;
@@ -20,3 +43,4 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+

6da55a2058fe10d3dcbd6e0e243783156257bd30
foveejong <jovefong62@gmail.com>
2024-02-29 17:44:29 +0800

    Assign correct final operator to opr variable

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const regex = new RegExp(/[0-9()+\-*=/.]/g);
 const display = document.querySelector("#display");
 const numButtons = document.querySelectorAll(".num");
 const oprButtons = document.querySelectorAll(".opr");
+const equals = document.querySelector(".equals");
 
 // for each button add eventlistener for button clicked and display
 numButtons.forEach(key => {
@@ -22,11 +23,18 @@ oprButtons.forEach(key => {
     key.addEventListener("click", e => {
         // upon first click of operators, store first variable value
         if (firstVar === "") firstVar = display.textContent;
-        //upon click, let first variable store values and populate display
-        opr = e.target.textContent;
         // limit opr used to only one
-        display.textContent = opr;
+        display.textContent = e.target.textContent;
+        //upon click, populate display with opr
+        opr = display.textContent;
+        console.log(opr);
     })
+})
+
+// when equals button is pressed, store second variable and run operation
+equals.addEventListener("click", e => {
+    // upon first click of equals, store second variable value
+    if (secondVar === "") secondVar = display.textContent;
 })
 
 

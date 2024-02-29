af37195ab5f9a9d9d49e8108f7c450b29a9cb976
foveejong <jovefong62@gmail.com>
2024-02-29 15:24:38 +0800

    Implement operate function to use basic calculator functions

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let firstVar, opr, secondVar, result;
+
+function operate(a, b, op) {
+    if (op === "+") return add(a, b)
+    if (op === "-") return subtract(a, b)
+    if (op === "*") return multiply(a, b)
+    if (op === "/") return divide(a, b)
+}
+
+
 function add(a, b) {
     return a + b;
 }

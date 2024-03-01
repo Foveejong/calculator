24ecda1cb4c895b211eb04f401beaaeebd63730f
foveejong <jovefong62@gmail.com>
2024-03-01 15:44:14 +0800

    Allow multiple calculations to be conducted
    When user clicks any operator button after both
    firstVar and secondVar is stored, operate is
    called and result is stored in firstVar. secondVar
    becomes an empty string.

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ numButtons.forEach(key => {
 
         //if opr exists and there is no secondVar yet, clear display to start displaying secondVar
         if (opr && secondVar === "") {
-            console.log("here");
             // remove operator from display
             display.textContent = "";  
             //populate display again
@@ -31,12 +30,24 @@ numButtons.forEach(key => {
 // for each opr button, only allow one opr to be used
 oprButtons.forEach(key => {
     key.addEventListener("click", e => {
+        // if user wants more than one operation
+        //execute first operation, firstvar = result, secondvar = "", display current result
+        if (opr && secondVar && firstVar) {
+            secondVar = display.textContent;
+            result = operate(+firstVar, +secondVar, opr);
+            console.log(result);
+            display.textContent = result;
+            firstVar = result;
+            secondVar = "";
+        }
+
         // upon first click of operators, store first variable value
         if (firstVar === "") firstVar = display.textContent;
         // limit opr used to only one
         display.textContent = e.target.textContent;
         //upon click, populate display with opr
         opr = display.textContent;
+
     })
 })
 
@@ -44,7 +55,16 @@ oprButtons.forEach(key => {
 equals.addEventListener("click", e => {
     // store second variable value
     secondVar = display.textContent;
-    display.textContent = operate(+firstVar, +secondVar, opr);
+    result = operate(+firstVar, +secondVar, opr);
+    display.textContent = result;
+
+    // reset all values to restart calculations
+    secondVar = "", firstVar = "", result = "", opr = "";
+    numButtons.forEach(key => {
+        key.addEventListener("click", e => {
+            display.textContent = e.target.textContent;
+        })
+    })
 })
 
 // if user presses valid numeric key, display value

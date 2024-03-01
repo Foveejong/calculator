70f50982a9d781b91e07ace43d7a3aea26579a21
foveejong <jovefong62@gmail.com>
2024-03-01 17:25:26 +0800

    Fix bugs
    Bug 1: after "=" is clicked, result was not used.
    Set up a boolean to determine if user is inputting
    an entirely new operation or continuing to use the
    previous result.
    
    Bug 2: wrong designation of firstVar and secondVar
    after decimal point is clicked. Omit an
    unnecessary conditional which checks if firstVar
    is an empty string.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let firstVar = "";
 let secondVar = "";
 let result = "";
 let opr = "";
+let newOp = false;
 
 // regex for numeric values and arithmetic operators
 const regex = new RegExp(/[0-9()+\-*=/.]/g);
@@ -9,10 +10,16 @@ const display = document.querySelector("#display");
 const numButtons = document.querySelectorAll(".num");
 const oprButtons = document.querySelectorAll(".opr");
 const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
 
 // for each button add eventlistener for button clicked and display
 numButtons.forEach(key => {
     key.addEventListener("click", e => {
+        // for entirely new operation, reset display to ""
+        if (newOp) {
+            display.textContent = "";
+            newOp = false;
+        }
         //upon click, populate display
         display.textContent += e.target.textContent;
 
@@ -32,39 +39,43 @@ oprButtons.forEach(key => {
     key.addEventListener("click", e => {
         // if user wants more than one operation
         //execute first operation, firstvar = result, secondvar = "", display current result
-        if (opr && secondVar && firstVar) {
+        if (secondVar != "" && firstVar != "" && opr != "") {
             secondVar = display.textContent;
             result = operate(+firstVar, +secondVar, opr);
             console.log(result);
-            display.textContent = result;
-            firstVar = result;
-            secondVar = "";
+            display.textContent = result, firstVar = result, secondVar = "";
         }
 
         // upon first click of operators, store first variable value
-        if (firstVar === "") firstVar = display.textContent;
+        firstVar = display.textContent;
         // limit opr used to only one
         display.textContent = e.target.textContent;
         //upon click, populate display with opr
         opr = display.textContent;
-
     })
 })
 
 // when equals button is pressed, store second variable and run operation
 equals.addEventListener("click", e => {
-    // store second variable value
-    secondVar = display.textContent;
-    result = operate(+firstVar, +secondVar, opr);
-    display.textContent = result;
+    //if user didnt input second value and opr, just return first value
+    if (secondVar === "" && opr === "") {
+        firstVar = display.textContent;
+    }
 
-    // reset all values to restart calculations
-    secondVar = "", firstVar = "", result = "", opr = "";
-    numButtons.forEach(key => {
-        key.addEventListener("click", e => {
-            display.textContent = e.target.textContent;
-        })
-    })
+    // if user inputs everything, carry on with usual operation
+    if (secondVar != "" && firstVar != "" && opr != "") {
+        // store second variable value
+        secondVar = display.textContent;
+        result = operate(+firstVar, +secondVar, opr);
+        display.textContent = result;
+        
+        // reset values to restart calculations
+        secondVar = "", firstVar = "", opr = "";
+    }
+
+    // if user press = immediately after operator
+    if (firstVar != "" && opr != "" && secondVar === "") alert("Please insert second value!")
+    newOp = true;
 })
 
 // if user presses valid numeric key, display value
@@ -93,4 +104,3 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
-
